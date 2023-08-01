74ea275c7164895c0ba71a0601e491f6cc287c2b
Kaloyan Stefanov <kokmarok@gmail.com>
2023-08-01 13:13:36 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Kaloyan Petkov on 1.08.23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
